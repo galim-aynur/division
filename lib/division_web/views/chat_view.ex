53b1eff783a26a8c2dbacb30cf344740eb7c32aa
Aynur Galimov <galim-aynur@ya.ru>
2019-10-18 21:57:45 +0500

    Update ChatView.avatar/1

--- lib/division_web/views/chat_view.ex
+++ lib/division_web/views/chat_view.ex
@@ -1,6 +1,8 @@
 defmodule DivisionWeb.ChatView do
   use DivisionWeb, :view
 
+  alias Division.Accounts.User
+
   def addressed_message?(message, username) do
     cond do
       String.contains?(message, "@#{username}") -> "addressed-message"
@@ -8,13 +10,13 @@ defmodule DivisionWeb.ChatView do
     end
   end
 
-  def avatar(user) do
-    if user.avatar do
-      Division.Avatar.url({user.avatar, user}, :thumb, signed: true)
-      |> img_tag(class: "avatar")
-    else
-      username = user.username |> String.first() |> String.upcase()
-      content_tag(:div, username, class: "avatar")
-    end
+  def avatar(%User{avatar: nil} = user) do
+    username = user.username |> String.first() |> String.upcase()
+    content_tag(:div, username, class: "avatar")
+  end
+
+  def avatar(%User{avatar: avatar} = user) do
+    Division.Avatar.url({avatar, user}, :thumb, signed: true)
+    |> img_tag(class: "avatar")
   end
 end
